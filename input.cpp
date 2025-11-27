42435727c804ec76dd9255ff6053089e478472dc
Yogaraj.S <78194098+yogarajjuju@users.noreply.github.com>
2025-11-27 08:39:40 +0000

    Tensooo

---
+++ input.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string name;
+    int age;
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << " Enter your age: ";
+    cin >> age;
+
+    cout<< "\nHello " << name << "! Your are " << age << " years old. " << endl;
+    return 0;
+}
