09986605cad16558528486d8ed03a7f034dd6c3c
Yogaraj.S <78194098+yogarajjuju@users.noreply.github.com>
2025-11-27 09:14:17 +0000

    hodo 3

---
+++ table.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout << "Enter the number";
+    cin >> num;
+
+    for(int i=1;i<=10;i++){
+        cout << num << " x " << i << " = "<< num* i << endl;
+    }
+    return 0;
+
+}
