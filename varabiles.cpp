8447226181340da7256c2919d2392c54af4290a5
Yogaraj.S <78194098+yogarajjuju@users.noreply.github.com>
2025-11-27 08:32:23 +0000

    Getsuga

---
+++ varabiles.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<string>
+using namespace std;
+int main() {
+    int age = 21;
+    double height = 6.4;
+    char grade ='O';
+    string name ="Juju";
+    bool isAlive =true;
+
+    cout << "Name " << name << endl;
+    cout << "Age " << age << endl;
+    cout << "Height " << height << endl;
+    cout << "Grade " << grade << endl;
+    cout << "Alive ? " << isAlive << endl;
+    
+    return 0;
+}
