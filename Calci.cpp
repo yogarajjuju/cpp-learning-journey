23187ba9444faac93fa994a05f8e6f68f20e0938
Yogaraj.S <78194098+yogarajjuju@users.noreply.github.com>
2025-11-27 08:46:14 +0000

    Sembonsakura

---
+++ Calci.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    int a , b ;
+
+    cout<< "Enter a";
+    cin>> a;
+    cout << "Enter b";
+    cin >> b;
+
+    cout <<" Addition" << a+b << endl;
+    cout << "Subraction" << a-b << endl;
+    cout << "Division" << a/b << endl;
+    cout << "Modulation" << a%b << endl;
+    return 0;
+}
